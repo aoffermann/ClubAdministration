5bd94197424de43696c50ec644a01b248f6b15db
Offermann Alexander <alexander.offermann@gmail.com>
2018-10-18 22:04:32 +0200

    add primary key for member entity
    Signed-off-by: Offermann Alexander <alexander.offermann@gmail.com>

--- src/main/java/eu/aosoft/club/admin/entity/Member.java
+++ src/main/java/eu/aosoft/club/admin/entity/Member.java
@@ -3,8 +3,7 @@ package eu.aosoft.club.admin.entity;
 import lombok.Data;
 import org.hibernate.envers.Audited;
 
-import javax.persistence.Entity;
-import javax.persistence.Table;
+import javax.persistence.*;
 
 /**
  * @author Offermann Alexander
@@ -15,4 +14,10 @@ import javax.persistence.Table;
 @Data
 @Audited
 public class Member {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "member_generator")
+    @SequenceGenerator(name = "member_generator", sequenceName = "member_seq", allocationSize = 1)
+    @Column(name = "MEMBER_ID")
+    private Long memberId;
 }
